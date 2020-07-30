89469339d0699b83b751ac7ff9be9e64aefbfebd
Koki-Chance <kouki2525suddenly@gmail.com>
2020-07-30 19:42:00 +0900

    rewrite_signIn_widget_that_I_deletedByMistake(#10)

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -17,6 +17,51 @@ class _SignInState extends State<SignIn> {
 
   @override
   Widget build(BuildContext context) {
-    
+    return Scaffold(
+      backgroundColor: Colors.brown[100],
+      appBar: AppBar(
+        backgroundColor: Colors.brown[400],
+        elevation: 0.0,
+        title: Text('Sign in to Brew Crew'),
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+        child: Form(
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 20.0),
+              TextFormField(
+                onChanged: (val) {
+                  setState(() => email = val);
+
+                }
+              ),
+              SizedBox(height: 20.0),
+              TextFormField(
+                // ↓ フォームに入力された文字が●として表示される
+                obscureText: true,
+                onChanged: (val) {
+                  setState(() => password = val);
+
+                }
+              ),
+              SizedBox(height: 20.0),
+              RaisedButton(
+                color: Colors.pink[400],
+                child: Text(
+                  'Sign in',
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () async {
+                  print(email);
+                  print(password);
+                  
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
