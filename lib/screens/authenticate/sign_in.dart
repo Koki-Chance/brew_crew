5282474d1ea7d3aee1b747efe977d5e31fc1175f
Koki-Chance <kouki2525suddenly@gmail.com>
2020-07-30 19:39:16 +0900

    create_register_widget(#10)

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -11,31 +11,12 @@ class _SignInState extends State<SignIn> {
 
   final AuthService _auth = AuthService();
 
+  // text field state
+  String email = '';
+  String password = '';
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown[100],
-      appBar: AppBar(
-        backgroundColor: Colors.brown[400],
-        elevation: 0.0,
-        title: Text('Sign in to Brew Crew'),
-      ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: RaisedButton(
-          child: Text('Sign in anonymously'),
-          onPressed: () async {
-            dynamic result = await _auth.signInAnon();
-            if (result == null) {
-              print('error signing in');
-            } else {
-              print('signed in');
-              print(result.uid);
-            }
-
-          }
-          ),
-      ),
-    );
+    
   }
 }
